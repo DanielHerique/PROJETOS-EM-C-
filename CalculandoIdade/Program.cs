3ca0266088c2afe79f0a3292d28a82c2db32d964
Daniel Henrique <161896580+DanielHerique@users.noreply.github.com>
2025-04-11 20:57:57 -0300

    Add files via upload

---
+++ CalculandoIdade/Program.cs
@@ -0,0 +1,13 @@
+﻿//Solicite que o usuário informe o seu ano de nascimento e calcule a idade considerando o ano atual como 2024
+
+class Program {
+    static void  Main(String[] args){
+
+        Console.WriteLine("Digite o seu ano de nascimento: ");
+        int anoNascimento = int.Parse(
+            Console.ReadLine());
+        int idade = 2024 - anoNascimento;
+        Console.WriteLine($"\nSua idade é de: {idade}\n");
+
+    }
+}
